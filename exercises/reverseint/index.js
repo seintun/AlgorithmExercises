452dcf2ec56d6d0a7c97b562cb6c7206faeb86dc
seintun <seintun.edu@gmail.com>
2019-02-16 18:54:00 -0800

    Sec05: reverseInt with if statement

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // reverse the array after converted to string
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  // assign negative if under 0
+  if (n < 0) {
+    return parseInt(reversed) * -1;
+  }
+  // otherwise return the value since positive
+  return parseInt(reversed);
+}
 
 module.exports = reverseInt;
