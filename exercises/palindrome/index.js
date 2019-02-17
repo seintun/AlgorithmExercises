57ce88dfd4538676f22b865036fd607742975e13
seintun <seintun.edu@gmail.com>
2019-02-16 18:42:15 -0800

    Sec4: Palindrome: split and count left&right gradually inwards

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,12 +8,21 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const reversed = str
-    .split("")
-    .reverse()
-    .join("");
-  // compare beforeReversal string with afterReversal string
-  return str === reversed;
+  // split string into array to access the value
+  return str.split("").every((char, i) => {
+    // compare values left & right inwards to validate bool true
+    // [2,5,7,5,2] -> 2 <=> 2, 5 <=> 5, 7 == 7
+    return char === str[str.length - i - 1];
+  });
 }
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split("")
+//     .reverse()
+//     .join("");
+//   // compare beforeReversal string with afterReversal string
+//   return str === reversed;
+// }
 
 module.exports = palindrome;
