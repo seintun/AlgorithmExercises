1aa7b9bd96f45f54fe10f82c66166f18cc101678
seintun <seintun.edu@gmail.com>
2019-02-16 20:08:26 -0800

    Sec06: maxChar ternary if char exists +1 or assign 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -11,11 +11,7 @@ function maxChar(str) {
   let maxChar = "";
 
   for (let char of str) {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
+    charMap[char] = charMap[char] ? +1 : 1;
   }
   for (let char in charMap) {
     if (charMap[char] > max) {
