f400b04b488621da66acc0b093d55e6ac878efde
seintun <seintun.edu@gmail.com>
2019-02-16 20:06:03 -0800

    Sec06: maxChar extract key:value of charMap and find the max

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,6 +7,8 @@
 
 function maxChar(str) {
   const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
   for (let char of str) {
     if (charMap[char]) {
@@ -15,7 +17,13 @@ function maxChar(str) {
       charMap[char] = 1;
     }
   }
-  console.log(charMap);
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
 
 module.exports = maxChar;
