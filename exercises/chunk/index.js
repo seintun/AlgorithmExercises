5040abf46d5b033133d26c6075bbde42a8755945
seintun <seintun.edu@gmail.com>
2019-02-17 23:45:56 -0800

    Sec08: chunk Solution-02, solved using while loop & slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,22 +9,37 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  // Create an empty array that holds sub-array of chunked
+  // Initialed chunked with empty array
   const chunked = [];
-  // Iterate through each element of array
-  for (let element of array) {
-    // Initialize last as the last element of chunked sub-array
-    const last = chunked[chunked.length - 1];
-    // Condition that check if there isn't last element or the length is === to size
-    if (!last || last.length === size) {
-      // a new sub-array with current elenment will be added to chunked
-      chunked.push([element]);
-    } else {
-      // Otherwise, push element into existing sub-array
-      last.push(element);
-    }
+  // Initialized index with 0, that will increment by size
+  let index = 0;
+  // Iterate with while until index not less than array.lenght
+  while (index < array.length) {
+    // slice the array by index & size, then push as sub-array
+    chunked.push(array.slice(index, index + size));
+    // Increment index by size
+    index += size;
   }
   return chunked;
 }
+
+// function chunk(array, size) {
+//   // Create an empty array that holds sub-array of chunked
+//   const chunked = [];
+//   // Iterate through each element of array
+//   for (let element of array) {
+//     // Initialize last as the last element of chunked sub-array
+//     const last = chunked[chunked.length - 1];
+//     // Condition that check if there isn't last element or the length is === to size
+//     if (!last || last.length === size) {
+//       // a new sub-array with current elenment will be added to chunked
+//       chunked.push([element]);
+//     } else {
+//       // Otherwise, push element into existing sub-array
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
 
 module.exports = chunk;
