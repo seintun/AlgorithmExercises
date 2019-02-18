38a605b98b9755465a4c04382807353f9e20412b
seintun <seintun.edu@gmail.com>
2019-02-18 00:54:53 -0800

    Sec10: Capitalize Solution02 with split, push into array, toUpperCase, slice

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,16 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// Solution 02
 function capitalize(str) {
-  const splitArr = str.split(" ");
-  let capitalized = "";
-  for (let char of splitArr) {
-    capitalized =
-      capitalized === ""
-        ? char[0].toUpperCase() + char.slice(1)
-        : (capitalized += " " + char[0].toUpperCase() + char.slice(1));
+  const words = [];
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
-  return capitalized;
+  
+  return words.join(' ');
 }
+
+// // Solution 01
+// function capitalize(str) {
+//   const splitArr = str.split(" ");
+//   let capitalized = "";
+//   for (let char of splitArr) {
+//     capitalized =
+//       capitalized === ""
+//         ? char[0].toUpperCase() + char.slice(1)
+//         : (capitalized += " " + char[0].toUpperCase() + char.slice(1));
+//   }
+//   return capitalized;
+// }
 
 module.exports = capitalize;
