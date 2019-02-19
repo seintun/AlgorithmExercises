311ffc30dab434619170d855129ab7e861a129ab
seintun <seintun.edu@gmail.com>
2019-02-18 20:00:19 -0800

    Solved pairsum exercise with solution

--- exercises/pairsum/index.js
+++ exercises/pairsum/index.js
@@ -6,6 +6,21 @@
 // pairsum([-99, -32, 0, 1, 32, 42, 77]) --> [-1, 1]
 // pairsum([-99, -36, -11, 1, 32, 42, 98]) --> false
 
-function pairsum(arr) {}
+function pairsum(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return false;
+}
 
 module.exports = pairsum;
