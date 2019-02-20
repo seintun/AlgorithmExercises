ffed855d7fb47c2999b7e0f5954244fe3e39b9b8
seintun <seintun.edu@gmail.com>
2019-02-19 16:35:04 -0800

    Added solution for biggestofcenter

--- exercises/biggestofcenter/index.js
+++ exercises/biggestofcenter/index.js
@@ -27,6 +27,71 @@
 //   [5, 8, 0, 3, 1]
 // ]
 
-function biggestofcenter(input) {}
+// Helper function that identify the center of the cube, return the xy cordinates alike
+function findCenter(cube) {
+  let vCenterIdx = Math.floor(cube.length / 2);
+  let hCenterIdx = Math.floor(cube[vCenterIdx].length / 2);
+  return { vertical: vCenterIdx, horizontal: hCenterIdx };
+}
+
+function biggestofcenter(input) {
+  const cIdx = findCenter(input);
+  // Using the helper function vertical index, identify upper, mi, lower subarray
+  const upperArr = input[cIdx.vertical - 1];
+  const midArr = input[cIdx.vertical];
+  const lowerArr = input[cIdx.vertical + 1];
+
+  // Function that will assign key:value pair to numMap object along with frequency counter
+  // accepts horizontal idx and current level of upper, mid, lower
+  const numMap = {};
+  const mapAssign = function(idx, level) {
+    numMap[level[idx]] = ++numMap[level[idx]] || 1;
+  };
+  // switch cases that assigns key:value of left, center, right of  current  idx
+  for (let i = 0; i < upperArr.length; i++) {
+    switch (i) {
+      case cIdx.horizontal:
+        mapAssign(i, upperArr);
+        break;
+      case cIdx.horizontal - 1:
+        mapAssign(i, upperArr);
+        break;
+      case cIdx.horizontal + 1:
+        mapAssign(i, upperArr);
+        break;
+    }
+  }
+  for (let i = 0; i < midArr.length; i++) {
+    switch (i) {
+      case cIdx.horizontal - 1:
+        mapAssign(i, midArr);
+        break;
+      case cIdx.horizontal + 1:
+        mapAssign(i, midArr);
+        break;
+    }
+  }
+  for (let i = 0; i < lowerArr.length; i++) {
+    switch (i) {
+      case cIdx.horizontal:
+        mapAssign(i, lowerArr);
+        break;
+      case cIdx.horizontal - 1:
+        mapAssign(i, lowerArr);
+        break;
+      case cIdx.horizontal + 1:
+        mapAssign(i, lowerArr);
+        break;
+    }
+  }
+  // Iteriate through the key value of biggest number and filter out for the biggest
+  let biggestNum = 0;
+  for (x in numMap) {
+    if (parseInt(x) > biggestNum) {
+      biggestNum = parseInt(x);
+    }
+  }
+  return biggestNum;
+}
 
 module.exports = biggestofcenter;
