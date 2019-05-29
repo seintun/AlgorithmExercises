b45d1629905ac0edd747244deab0cb2f2284f2ce
seintun <seintun.edu@gmail.com>
2019-05-28 22:54:44 -0700

    Added Regex solution for vowels kata

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,16 +8,22 @@
 //   vowels('Why?') --> 0
 
 // Iterative solution
+// function vowels(str) {
+//   let count = 0;
+//   const vowelChecker = ["a", "e", "i", "o", "u"];
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowelChecker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// Regex solution
 function vowels(str) {
-  let count = 0;
-  const vowelChecker = ["a", "e", "i", "o", "u"];
-
-  for (let char of str.toLowerCase()) {
-    if (vowelChecker.includes(char)) {
-      count++;
-    }
-  }
-  return count;
+  // g doesn't stop upon first match, i disregards case-sensitive
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
-
 module.exports = vowels;
